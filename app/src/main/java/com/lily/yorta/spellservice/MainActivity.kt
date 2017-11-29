c52d2cfa4db4fa40e7a4aebb8d35917c9596f608
youtangai <youtanagai@gmail.com>
2017-11-29 17:48:03 +0900

    [add]implement aidl caller

--- app/src/main/java/com/lily/yorta/spellservice/MainActivity.kt
+++ app/src/main/java/com/lily/yorta/spellservice/MainActivity.kt
@@ -1,9 +1,28 @@
 package com.lily.yorta.spellservice
 
+import android.content.ComponentName
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "yorta.lily.aidl"
+    var mIMyAidlInterface: IMyAidlInterface? = null
+
+    private val mConnection = object: ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName?, service: IBinder?){
+            mIMyAidlInterface = IMyAidlInterface.Stub.asInterface(service)
+        }
+
+        override fun onServiceDisconnected(className: ComponentName?) {
+            Log.e(TAG, "Service has unexpectedly disconnected")
+            mIMyAidlInterface = null
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/lily/yorta/spellservice/SpellService.kt
+++ app/src/main/java/com/lily/yorta/spellservice/SpellService.kt
@@ -6,15 +6,15 @@ import android.os.IBinder
 
 class SpellService : Service() {
 
-    private val mBinder = object: IMyAidlInterface.Stub() {
-        override fun spellMagic(magic: String?) {
-            print(magic)
-        }
-    }
-
     override fun onCreate() {
         super.onCreate()
     }
 
     override fun onBind(intent: Intent): IBinder? { return mBinder }
+
+    private val mBinder = object: IMyAidlInterface.Stub() {
+        override fun spellMagic(magic: String?) {
+            print(magic)
+        }
+    }
 }
