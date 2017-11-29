178d0f8d03e6ed9cf04654ea1d7015782d671f47
youtangai <youtanagai@gmail.com>
2017-11-29 16:23:33 +0900

    [add]implement interface

---
+++ app/src/main/java/com/lily/yorta/spellservice/SpellService.kt
@@ -0,0 +1,15 @@
+package com.lily.yorta.spellservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SpellService : Service() {
+    private val mBinder = MyAidl()
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onBind(intent: Intent): IBinder? { return mBinder }
+}
